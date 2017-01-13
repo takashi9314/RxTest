5a293fdcb22fe26538607bde9abba876b715add2
松本 隆 <takashidoesitright@gmail.com>
2017-01-13 23:42:10 +0900

    Initial Commit

---
+++ RxTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxTest
+//
+//  Created by 松本 隆 on 2017/01/13.
+//  Copyright © 2017年 Favy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
