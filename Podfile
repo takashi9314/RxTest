d7465b7bd41a717f42ec8de76023512238a06246
松本 隆 <takashidoesitright@gmail.com>
2017-01-14 01:30:56 +0900

    initialCommit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RxTest
+  pod 'RxSwift',    '~> 3.1.0'
+  pod 'RxCocoa',    '~> 3.1.0'
+
+  target 'RxTestTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RxTestUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- RxTest/ViewController.swift
+++ RxTest/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    
+    var disposeBag = DisposeBag()
+    
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var rxSwitch: UISwitch!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        button.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self!.label.text = "ButtonTapped"
+            })
+            .addDisposableTo(disposeBag)
+        
+        rxSwitch.rx.isOn
+            .subscribe(onNext: { [weak self] bool in
+                self!.label.text = bool ? "SwitchOn" : "SwitchOff"
+            }).addDisposableTo(disposeBag)
+        
+        stepper.rx.value
+            .subscribe(onNext: { [weak self] value in
+                self!.label.text = String(value)
+                }).addDisposableTo(disposeBag)
+        
+        textField.rx.text
+            .subscribe(onNext: { [weak self] text in
+                self!.label.text = text
+            }).addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +54,3 @@ class ViewController: UIViewController {
 
 
 }
-
